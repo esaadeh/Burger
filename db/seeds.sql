bb7fbbbed02e98e452d9cd7a647fca3106770d0e
EKS <saadeh.emad@gmail.com>
2018-12-19 05:47:06 -0800

    Initial commit...file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+
+INSERT INTO burgers (burger) VALUES ('Kangaroo Burger', false);
+INSERT INTO burgers (burger) VALUES ('Bison Burger', false);
+INSERT INTO burgers (burger) VALUES ('Ostrich Burger', false);
